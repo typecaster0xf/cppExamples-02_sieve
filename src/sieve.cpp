06facc85770366f446c7a8edc92204f3e3bd326a
N. Streifling <N.Streifling@gmail.com>
2015-11-04 17:20:53 -0800

    Filled in the main sieving algorithm.

--- src/sieve.cpp
+++ src/sieve.cpp
@@ -1,4 +1,11 @@
+#include <cassert>
+#include <cmath>
 #include "sieve.h"
+
+unsigned long findNextPrime(const unsigned long currentPrime,
+		const vector<bool> &sieve);
+
+//===============================================
 
 Eretosthenes::Eretosthenes(const unsigned long maxValueInSieve):
 sieve(maxValueInSieve >> 1, true),
@@ -8,6 +15,30 @@ sieveHasBeenRun(false)
 vector<unsigned long> 
 Eretosthenes::runSieve()
 {
+	const unsigned long sievingLimit = static_cast<unsigned long>(
+			ceil(sqrt((sieve.size() << 1) | 1)));
+	
+	for(unsigned long prime = 3; prime < sievingLimit;
+			prime = findNextPrime(prime, sieve))
+	{
+		for(unsigned long j = prime - 1, inc = prime >> 1;
+				j < sieve.size(); j += inc)
+			sieve[j] = false;
+	}
+	
+	//TODO
 	vector<unsigned long> returnValue;
 	return returnValue;
+}
+
+//===============================================
+
+unsigned long findNextPrime(const unsigned long currentPrime,
+		const vector<bool> &sieve)
+{
+	for(unsigned long j = currentPrime >> 1; j < sieve.size(); j++)
+		if(sieve[j])
+			return (j << 1) | 1;
+	
+	assert(false);
 }
