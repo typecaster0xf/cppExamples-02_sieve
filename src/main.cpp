fe9079f382c5dc7c5061cba2a10de8ed2dd42cf6
N. Streifling <N.Streifling@gmail.com>
2015-11-04 20:02:28 -0800

    Filled in the main function for the program, but it isn't creating the output file for some reason.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <cstdlib>
 #include <fstream>
 #include <iostream>
 #include "sieve.h"
@@ -6,7 +7,25 @@ using namespace std;
 
 int main(const int argc, const char* argv[])
 {
-	cout << "This is the main function." << endl;
+	if(argc < 3)
+	{
+		cout << "Usage: " << argv[0]
+				<< " <max_value> <output_file_name>" << endl;
+		return 0;
+	}
+	
+	const unsigned long maxValue       = atoi(argv[1]);
+	const char*         outputFileName = argv[2];
+	
+	//--
+	
+	Eretosthenes primeGenerator(maxValue);
+	vector<unsigned long> primes = primeGenerator.runSieve();
+	
+	fstream outputFile(outputFileName);
+	for(unsigned long j = 0; j < primes.size(); j++)
+		outputFile << primes[j] << '\n';
+	outputFile.close();
 	
 	return 0;
 }
